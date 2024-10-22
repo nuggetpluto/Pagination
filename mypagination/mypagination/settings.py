cdce811d77123048d9581fd5dc54a7ef30f5ad0f
nuggetpluto <korshikov1501@gmail.com>
2024-10-22 18:09:09 +0300

    Настраиваем СУБД postgre в django

--- mypagination/mypagination/settings.py
+++ mypagination/mypagination/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'mypagination.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Eugen_db',
+        'USER': 'postgres',
+        'PASSWORD': 'killeu1501',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
